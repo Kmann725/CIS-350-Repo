f23e3818c20932eaa570e1c69edb445847fbef61
Kmann725 <khmanning@mail.bradley.edu>
2022-09-25 18:07:16 -0500

    Added Challenge 3

---
+++ Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * Kyle Manning
+ * Challenge 3
+ * Allows the player to float with space bar and handles
+ * collision events
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+    private bool isLowEnough = true;
+
+    public float floatForce;
+    public float upperLimit;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+    private ScoreManager sm;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip bounceSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        sm = GameObject.FindObjectOfType<ScoreManager>();
+
+        if (Physics.gravity.y > -10)
+        {
+            Physics.gravity *= gravityModifier;
+        }
+        playerAudio = GetComponent<AudioSource>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > upperLimit)
+        {
+            isLowEnough = false;
+        }
+        else
+        {
+            isLowEnough = true;
+        }
+
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver && isLowEnough)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce * 15);
+            playerAudio.PlayOneShot(bounceSound, 1.0f);
+        }
+
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            sm.score++;
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+        }
+
+    }
+
+}
---
+++ Challenge3/Assets/Challenge 3/Scripts/ScoreManager.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Kyle Manning
+ * Challenge 3
+ * Updates score counter, displays win and lose messages, and allows the player
+ * to restart on game over
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+
+    private PlayerControllerX pcx;
+
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pcx = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerX>();
+
+        scoreText.text = "Score: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pcx.gameOver)
+        {
+            scoreText.text = "Score: " + score;
+        }
+
+        if (pcx.gameOver)
+        {
+            scoreText.text = "You Lose!" + "\n" + "Press R to Try Again!";
+        }
+
+        if (score >= 15)
+        {
+            pcx.gameOver = true;
+
+            scoreText.text = "You Win!" + "\n" + "Press R to Try Again!";
+        }
+
+        if (pcx.gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
