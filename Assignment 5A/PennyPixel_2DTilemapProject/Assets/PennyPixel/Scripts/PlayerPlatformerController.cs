eb33824b28acaf98ed90d110282611cb01d5d958
Kmann725 <khmanning@mail.bradley.edu>
2022-10-10 02:53:43 -0500

    Added Assignment 5A

---
+++ Assignment 5A/PennyPixel_2DTilemapProject/Assets/PennyPixel/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * Kyle Manning
+ * Assignment 5A - 2D game
+ * Handles player movement and animation states
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerPlatformerController : PhysicsObject {
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    private bool gameOver = false;
+
+    public Text scoreText;
+    public GameObject winText;
+
+    public int gems = 0;
+
+    // Use this for initialization
+    void Awake () 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer> (); 
+        animator = GetComponent<Animator> ();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        if (transform.position.y < -23)
+        {
+            gameOver = true;
+            winText.GetComponent<Text>().text = "You Lose!\nPress R to Try Again!";
+            winText.SetActive(true);
+        }
+
+        scoreText.text = "Gems: " + gems;
+
+        if (!gameOver)
+        {
+            Vector2 move = Vector2.zero;
+
+            move.x = Input.GetAxis("Horizontal");
+
+            if (Input.GetButtonDown("Jump") && grounded)
+            {
+                velocity.y = jumpTakeOffSpeed;
+            }
+            else if (Input.GetButtonUp("Jump"))
+            {
+                if (velocity.y > 0)
+                {
+                    velocity.y = velocity.y * 0.5f;
+                }
+            }
+
+            if (move.x > 0.01f)
+            {
+                if (spriteRenderer.flipX == true)
+                {
+                    spriteRenderer.flipX = false;
+                }
+            }
+            else if (move.x < -0.01f)
+            {
+                if (spriteRenderer.flipX == false)
+                {
+                    spriteRenderer.flipX = true;
+                }
+            }
+
+            animator.SetBool("grounded", grounded);
+            animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+            animator.SetFloat("velocityY", velocity.y);
+
+            targetVelocity = move * maxSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && gameOver)
+        {
+            SceneManager.LoadScene("TilemapStart");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("End"))
+        {
+            gameOver = true;
+            winText.SetActive(true);
+            animator.SetFloat("velocityX", 0);
+        }
+    }
+}
