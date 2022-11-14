45eddf7af941dbfa6952c2973020a6d9c429106c
Kmann725 <khmanning@mail.bradley.edu>
2022-11-13 21:11:38 -0600

    Added Challenge 4

---
+++ Challenge4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * Kyle Manning
+ * Challenge 4
+ * Handles player movement and collisions
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float speed = 500;
+    private GameObject focalPoint;
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public GameObject infoText;
+    public ParticleSystem smoke;
+    public int powerUpDuration = 5;
+    public float speedBoost = 50;
+    public bool gameOver = false;
+
+    private float normalStrength = 10; // how hard to hit enemy without powerup
+    private float powerupStrength = 25; // how hard to hit enemy with powerup
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+        Time.timeScale = 0;
+    }
+
+    void Update()
+    {
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+
+        // Set powerup indicator position to beneath player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.timeScale == 1)
+        {
+            playerRb.AddForce(focalPoint.transform.forward * speedBoost, ForceMode.Impulse);
+            smoke.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && infoText)
+        {
+            infoText.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && gameOver)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position; 
+           
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+
+
+        }
+    }
+
+
+
+}
---
+++ Challenge4/Assets/Challenge 4/Scripts/WaveTracker.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Kyle Manning
+ * Challenge 4
+ * Keeps track of and displays the wave number
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveTracker : MonoBehaviour
+{
+    private SpawnManagerX sm;
+    private Text waveText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+        waveText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waveText.text = "Wave " + sm.waveCount;
+    }
+}
