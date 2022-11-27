af78f77f74d5721471b1ce742db785d8f1eb6a1d
Kmann725 <khmanning@mail.bradley.edu>
2022-11-27 16:12:00 -0600

    Added Prototype 5

---
+++ Prototype5/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Kyle Manning
+ * Prototype 5
+ * Sets the game's difficulty
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private GameManager gameManager;
+    private Button button;
+
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    void SetDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+    }
+}
---
+++ Prototype5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Kyle Manning
+ * Prototype 5
+ * Spawns targets and handles game states
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+
+    private float spawnRate = 1.0f;
+    private int score;
+
+    public bool isGameActive;
+
+    // Start is called before the first frame update
+    public void StartGame(int difficulty)
+    {
+        titleScreen.SetActive(false);
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+        UpdateScore(0);
+        StartCoroutine(SpawnTarget());
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
